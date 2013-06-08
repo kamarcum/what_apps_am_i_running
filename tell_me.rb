c3843c99a7aee31fc7deacd1600ca90b957f5fb3
Keith Marcum <marcum.keith@gmail.com>
2013-06-08 12:21:43 -0500

    Separate queries from reporting

--- tell_me.rb
+++ tell_me.rb
@@ -6,7 +6,8 @@ class InfoDocument < Nokogiri::XML::SAX::Document
     @in_key = false
     @in_value = false
     @in_version = false
-    @version = nil
+    @cf_bundle_version = nil
+    @cf_bundle_short_version_string = nil
   end
   def start_element(name, attributes=[])
     case name
@@ -27,16 +28,25 @@ class InfoDocument < Nokogiri::XML::SAX::Document
   def characters(characters)
     if @in_key
       if characters == "CFBundleVersion" || characters == "CFBundleShortVersionString"
-        @in_version = true
+        @in_version = characters
       end
     elsif @in_value && @in_version
-      @version = characters
-      @in_version = false
+      if @in_version == "CFBundleVersion"
+        @cf_bundle_version = characters
+        @in_version = false
+      elsif @in_version == "CFBundleShortVersionString"
+        @cf_bundle_short_version_string = characters
+        @in_version = false
+      end
     end
+  end
+  def version
+    @cf_bundle_version || @cf_bundle_short_version_string
   end
 end
 
-def print_app_versions_for(base_path)
+def find_app_versions_for(base_path)
+  results = {}
   Dir.foreach(base_path) do |path|
     total_path = "#{base_path}/#{path}"
     if path =~ /\.app$/
@@ -48,16 +58,23 @@ def print_app_versions_for(base_path)
           parser = Nokogiri::XML::SAX::Parser.new(document)
           parser.parse info
           if document.version
-            puts "#{path} is version #{document.version}"
+            results[path] = document.version
             break
           end
         end
       end
     elsif path != "." && path != ".." && Dir.exists?(total_path)
-      print_app_versions_for total_path
+      results.merge find_app_versions_for total_path
     end
   end
+  results
 end
 
-print_app_versions_for "/Applications"
-print_app_versions_for "#{ENV['HOME']}/Applications"
+results = find_app_versions_for "/Applications"
+user_apps_dir = "#{ENV['HOME']}/Applications"
+if Dir.exists? user_apps_dir
+  results.merge find_app_versions_for "#{ENV['HOME']}/Applications"
+end
+results.each_pair do |key, value|
+  puts "#{key} => #{value}"
+end
